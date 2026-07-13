3aeefeb736c49992bb01093649c240d301100b72
ivanbyone <ivanbyone@gmail.com>
2026-07-13 23:07:34 +0300

    feat: handle Domain error AlreadyExistsException

--- chat-backend/src/main/java/io/ivanbyone/chat_backend/config/exception/GlobalExceptionHandler.java
+++ chat-backend/src/main/java/io/ivanbyone/chat_backend/config/exception/GlobalExceptionHandler.java
@@ -1,17 +1,26 @@
 package io.ivanbyone.chat_backend.config.exception;
 
 import io.ivanbyone.chat_backend.adapter.web.dto.ResponseDto;
+import io.ivanbyone.chat_backend.application.exception.AlreadyExistsException;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Log4j2
-@ControllerAdvice
+@RestControllerAdvice
 @SuppressWarnings("unused")
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseDto<?> handleAlreadyExistsException(AlreadyExistsException error) {
+        String message = error.getLocalizedMessage();
+        log.warn(message);
+        return ResponseDto.error(message, HttpStatus.CONFLICT.value());
+    }
 
     @ExceptionHandler(OptimisticLockingFailureException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
